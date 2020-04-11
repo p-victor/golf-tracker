f8113254c8655164cb6fc3634840a0067c0fe54f
NaSinSa <andy.k.oh@hotmail.com>
2020-04-10 21:56:12 -0400

    add gmap link on postal code

--- front-end/src/components/GolfCourse/GolfCourse.js
+++ front-end/src/components/GolfCourse/GolfCourse.js
@@ -5,7 +5,7 @@ export default function GolfCourse(props) {
   return (
     <article className="golfCourse">
       <div className="name">{props.name}</div>
-      <div className="postal__code">{props.postal_code}</div>
+      <div className="postal__code"><a href={`https://google.com/maps/place/${props.postal_code}`}>{props.postal_code}</a></div>
       <div className="phone__number">{props.phone_number}</div>
       <div className="website">{props.website}</div>
       <div className="no__results">{props.results}</div>
