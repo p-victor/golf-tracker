6f4d2e8c523b36f7926fa528390fe4f4af9bdc19
Victor Poirier <poiriervictor99@gmail.com>
2020-04-13 18:11:32 -0400

    remade the searchbar states

--- front-end/src/components/Search/Search.js
+++ front-end/src/components/Search/Search.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-import useApp from "../../hooks/useApp";
+import useSearch from "../../hooks/useSearch";
 import SearchBar from "../SearchBar/SearchBar";
 import Results from "../Results/Results";
+import useSearchBar from "../../hooks/useSearch";
 
 export default function Search(props) {
 
-  const { setKeyword, results } = useApp();
+  const [state, setState] = useSearch();
 
   return (
     <main>
-      <SearchBar key="0" onSearch={keyword => setKeyword(keyword)} />
-      <Results key="1" results={results} />
+      <SearchBar key="0" search={state.search} onSearch={search => setState(prev => ({...prev, search}))} />
+      <Results key="1" results={state.results} />
     </main>
   );
 }
---
+++ front-end/src/hooks/useSearch.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function useSearchBar(props) {
+  const [state, setState] = useState({ results: [], search: "" });
+
+  useEffect(() => {
+    axios
+      .get(`/api/courses/${state.search}`)
+      .then(res => setState(prev => ({...prev, results: res.data[0]})))
+  }, [state.search])
+
+  return [state, setState]
+}
